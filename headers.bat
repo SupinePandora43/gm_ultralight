8074a81462978f28fc35e4d24122aab13f334208
SupinePandora43 <supinepandoraggg@gmail.com>
2020-05-26 07:57:54 +0500

    main

---
+++ headers.bat
@@ -0,0 +1,5 @@
+curl https://raw.githubusercontent.com/Facepunch/gmod-module-base/development/include/GarrysMod/Lua/Interface.h -o include/GarrysMod/Lua/Interface.h
+curl https://raw.githubusercontent.com/Facepunch/gmod-module-base/development/include/GarrysMod/Lua/LuaBase.h -o include/GarrysMod/Lua/LuaBase.h
+curl https://raw.githubusercontent.com/Facepunch/gmod-module-base/development/include/GarrysMod/Lua/SourceCompat.h -o include/GarrysMod/Lua/SourceCompat.h
+curl https://raw.githubusercontent.com/Facepunch/gmod-module-base/development/include/GarrysMod/Lua/Types.h -o include/GarrysMod/Lua/Types.h
+curl https://raw.githubusercontent.com/Facepunch/gmod-module-base/development/include/GarrysMod/Lua/UserData.h -o include/GarrysMod/Lua/UserData.h
