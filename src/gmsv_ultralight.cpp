5a051c854e3ca3f52edd61eab3e514c0fc092755
Deyan Dobromirov <dvdvideo123@gmail.com>
2020-05-26 11:49:04 +0300

    Update gmsv_ultralight.cpp

--- src/gmsv_ultralight.cpp
+++ src/gmsv_ultralight.cpp
@@ -24,10 +24,10 @@ void Msg(const char*, ...) {}
 
 class MyApp : public LoadListener {
 	RefPtr<Renderer> renderer_;
-  RefPtr<View> view_;
-  lua_State *lua_ = nullptr;
+	RefPtr<View> view_;
+	lua_State *lua_ = nullptr;
 	bool done_ = false;
-  uint8_t* adr_ = nullptr;
+	uint8_t* adr_ = nullptr;
 public:
 	MyApp() {
 		Msg("c++: MyApp: Creating...\n");
@@ -51,95 +51,95 @@ public:
 
 		//Msg("c++: MyApp: view loaded url\n");
 	}
-  
-  void SetState(lua_State *L)
-  {
-    lua_ = (lua_State *)L;
-  }
-  
-  void SetBitmap()
-  {
-    if(view_ != nullptr)
-    {
-      adr_ = (uint8_t*)view_->bitmap()->LockPixels();
-    }
-  }
-  
+	
+	void SetState(lua_State *L)
+	{
+		lua_ = (lua_State *)L;
+	}
+	
+	void SetBitmap()
+	{
+		if(view_ != nullptr)
+		{
+			adr_ = (uint8_t*)view_->bitmap()->LockPixels();
+		}
+	}
+	
 	void SetURL()
-  {
+	{
 		this->SetURL("https://google.com");
 	}
-  
+	
 	void SetURL(String url) {
 		view_->LoadURL(url);
 	}
-  
-  void GetField(const char *name)
-  {
-    if(lua_ != nullptr && name != nullptr)
-    {
-      lua_->GetField(-1, name);
-    }
-  }
-  
-  void SetDrawColor(uint32_t i)
-  {
-    if(lua_ != nullptr && adr_ != nullptr)
-    {
-      GetField("SetDrawColor")
-      lua_->PushNumber(adr_[i + 2]);//R
-      lua_->PushNumber(adr_[i + 1]);//G
-      lua_->PushNumber(adr_[i + 0]);//B
-      lua_->PushNumber(adr_[i + 3]);//A
-      lua_->Call(4, 0);
-    }
-  }
-  
-  void DrawPixel(uint16_t x, uint16_t y)
-  {
-    if(lua_ != nullptr)
-    {
-      GetField("DrawRect")
-      lua_->PushNumber(x);
-      lua_->PushNumber(y);
-      lua_->PushNumber(1);
-      lua_->PushNumber(1);
-      lua_->Call(4, 0);
-    }
-  }
-  
+	
+	void GetField(const char *name)
+	{
+		if(lua_ != nullptr && name != nullptr)
+		{
+			lua_->GetField(-1, name);
+		}
+	}
+	
+	void SetDrawColor(uint32_t i)
+	{
+		if(lua_ != nullptr && adr_ != nullptr)
+		{
+			GetField("SetDrawColor")
+			lua_->PushNumber(adr_[i + 2]);//R
+			lua_->PushNumber(adr_[i + 1]);//G
+			lua_->PushNumber(adr_[i + 0]);//B
+			lua_->PushNumber(adr_[i + 3]);//A
+			lua_->Call(4, 0);
+		}
+	}
+	
+	void DrawPixel(uint16_t x, uint16_t y)
+	{
+		if(lua_ != nullptr)
+		{
+			GetField("DrawRect")
+			lua_->PushNumber(x);
+			lua_->PushNumber(y);
+			lua_->PushNumber(1);
+			lua_->PushNumber(1);
+			lua_->Call(4, 0);
+		}
+	}
+	
 	virtual ~MyApp()
-  {
+	{
 		view_ = nullptr;
 		renderer_ = nullptr;
-    adr_ = nullptr;
+		adr_ = nullptr;
 	}
 
 	void Run()
-  {
+	{
 		std::cout << "Starting Run(), waiting for page to load..." << std::endl;
 		while (!done_)
 			renderer_->Update();
 		std::cout << "Finished." << std::endl;
 	}
-  
-  void Draw()
-  {
-    GetField("surface")
-    uint16_t w = view_->width();
-    uint16_t h = view_->height();
-    for (uint16_t y = 0; y < h; y++)
-    {
-      for (uint16_t x = 0; x < w; x++)
-      {
-        SetDrawColor((uint32_t)x * 4 + (uint32_t)y * w);
-        DrawPixel(x, y);
-      }
-    }
-  }
-  
+	
+	void Draw()
+	{
+		GetField("surface")
+		uint16_t w = view_->width();
+		uint16_t h = view_->height();
+		for (uint16_t y = 0; y < h; y++)
+		{
+			for (uint16_t x = 0; x < w; x++)
+			{
+				SetDrawColor((uint32_t)x * 4 + (uint32_t)y * w);
+				DrawPixel(x, y);
+			}
+		}
+	}
+	
 	virtual void OnFinishLoading(ultralight::View* caller)
-  {
+	{
 		Msg("c++: Page loaded");
 		renderer_->Render();
 		view_->bitmap()->WritePNG("result.png");
@@ -160,13 +160,13 @@ LUA_FUNCTION(RenderImage) {
 	Msg("c++: app.SetURL() is done\n");
 	app.Run();
 	Msg("c++: app.Run() is done\n");
-  app.SetBitmap()
-  Msg("c++: app.SetBitmap() is done\n");
-  app.SetState(LUA);
-  Msg("c++: app.SetState() is done\n");
+	app.SetBitmap()
+	Msg("c++: app.SetBitmap() is done\n");
+	app.SetState(LUA);
+	Msg("c++: app.SetState() is done\n");
 	//adress = view_->bitmap()->raw_pixels();
 	//size_t sizeofadress = sizeof((uint8_t)adress);
-  app.Draw();
+	app.Draw();
 	LUA->Pop();
 	return 0;
 }
