9a496b3788b2edc97d6b8819bffe229a9ab7cce8
SupinePandora43 <supinepandoraggg@gmail.com>
2020-05-31 10:36:18 +0500

    maybe...

--- src/gmsv_ultralight.cpp
+++ src/gmsv_ultralight.cpp
@@ -15,7 +15,7 @@
 using namespace GarrysMod::Lua;
 using namespace ultralight;
 
-typedef void* (__cdecl* MsgFn)(const char*, ...);
+typedef void* (__stdcall* MsgFn)(const char*, ...);
 
 MsgFn Msg;
 class MyApp : public LoadListener {
