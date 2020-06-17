6d98ed08c7a469408dd59ec2cf25e9d59126f51a
SupinePandora43 <supinepandoraggg@gmail.com>
2020-06-17 14:20:10 +0500

    bruh

--- src/ultralight_test.cpp
+++ src/ultralight_test.cpp
@@ -136,34 +136,6 @@ public:
 Shm* ul_io_rpc;
 Shm* ul_i_image;
 Shm* ul_o_url;
-#include <windows.h>
-VOID startup(LPCTSTR lpApplicationName)
-{
-	// additional information
-	STARTUPINFO si;
-	PROCESS_INFORMATION pi;
-
-	// set the size of the structures
-	ZeroMemory(&si, sizeof(si));
-	si.cb = sizeof(si);
-	ZeroMemory(&pi, sizeof(pi));
-
-	// start the program up
-	CreateProcess(lpApplicationName,   // the path
-		"",        // Command line
-		NULL,           // Process handle not inheritable
-		NULL,           // Thread handle not inheritable
-		FALSE,          // Set handle inheritance to FALSE
-		0,              // No creation flags
-		NULL,           // Use parent's environment block
-		NULL,           // Use parent's starting directory 
-		&si,            // Pointer to STARTUPINFO structure
-		&pi             // Pointer to PROCESS_INFORMATION structure (removed extra parentheses)
-	);
-	// Close process and thread handles. 
-	CloseHandle(pi.hProcess);
-	CloseHandle(pi.hThread);
-}
 void threadStarter() {
 	std::cout << std::system("ultralight_renderer.exe") << std::endl;
 }
