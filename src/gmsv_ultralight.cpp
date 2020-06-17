696d9e85626ca7d76e16a0321f3a4e983e6d8849
SupinePandora43 <supinepandoraggg@gmail.com>
2020-06-17 23:19:41 +0500

    it works on my machine

--- src/gmsv_ultralight.cpp
+++ src/gmsv_ultralight.cpp
@@ -17,123 +17,8 @@
 #include <windows.h> // CreateFileMapping
 #include <io.h>
 #endif
+#include "shoom/shm.h" // Shm
 
-enum ShoomError {
-	kOK = 0,
-	kErrorCreationFailed = 100,
-	kErrorMappingFailed = 110,
-	kErrorOpeningFailed = 120,
-};
-class Shm {
-	std::string path_;
-	uint8_t* data_ = nullptr;
-	size_t size_ = 0;
-#if defined(_WIN32)
-	HANDLE handle_;
-#else
-	int fd_ = -1;
-#endif
-public:
-	//Shm(std::string path, size_t size) : path_(path), size_(size) {};
-	Shm(std::string path, size_t size) {
-#ifdef WIN32
-		path_ = path;
-#else
-		path_ = "/" + path;
-#endif
-		size_ = size;
-	};
-	ShoomError Create() { return CreateOrOpen(true); }
-	ShoomError Open() { return CreateOrOpen(false); }
-	uint8_t* Data() { return data_; }
-	ShoomError CreateOrOpen(bool create) {
-#ifdef WIN32
-		if (create) {
-			DWORD size_high_order = 0;
-			DWORD size_low_order = static_cast<DWORD>(size_);
-			handle_ = CreateFileMappingA(INVALID_HANDLE_VALUE,  // use paging file
-				NULL,                  // default security
-				PAGE_READWRITE,        // read/write access
-				size_high_order, size_low_order,
-				path_.c_str()  // name of mapping object
-			);
-			if (!handle_) {
-				return kErrorCreationFailed;
-			}
-		}
-		else {
-			handle_ = OpenFileMappingA(FILE_MAP_READ,  // read access
-				FALSE,          // do not inherit the name
-				path_.c_str()   // name of mapping object
-			);
-
-			if (!handle_) {
-				return kErrorOpeningFailed;
-			}
-		}
-		DWORD access = create ? FILE_MAP_ALL_ACCESS : FILE_MAP_READ;
-		data_ = static_cast<uint8_t*>(MapViewOfFile(handle_, access, 0, 0, size_));
-		if (!data_) {
-			return kErrorMappingFailed;
-		}
-		return kOK;
-#else
-		if (create) {
-			int ret = shm_unlink(path_.c_str());
-			if (ret < 0) {
-				if (errno != ENOENT) {
-					return kErrorCreationFailed;
-				}
-			}
-		}
-		int flags = create ? (O_CREAT | O_RDWR) : O_RDONLY;
-		fd_ = shm_open(path_.c_str(), flags, 0755);
-		if (fd_ < 0) {
-			if (create) {
-				return kErrorCreationFailed;
-			}
-			else {
-				return kErrorOpeningFailed;
-			}
-		}
-		if (create) {
-			int ret = ftruncate(fd_, size_);
-			if (ret != 0) {
-				return kErrorCreationFailed;
-			}
-		}
-		int prot = create ? (PROT_READ | PROT_WRITE) : PROT_READ;
-		auto memory = mmap(nullptr,     // addr
-			size_,       // length
-			prot,        // prot
-			MAP_SHARED,  // flags
-			fd_,         // fd
-			0            // offset
-		);
-		if (memory == MAP_FAILED) {
-			return kErrorMappingFailed;
-		}
-		data_ = static_cast<uint8_t*>(memory);
-		if (!data_) {
-			return kErrorMappingFailed;
-		}
-		return kOK;
-#endif
-	}
-	~Shm() {
-#ifndef WIN32
-		munmap(data_, size_);
-		close(fd_);
-		shm_unlink(path_.c_str());
-#else
-		if (data_) {
-			UnmapViewOfFile(data_);
-			data_ = nullptr;
-		}
-		CloseHandle(handle_);
-#endif
-	}
-};
 using namespace GarrysMod::Lua;
 
 typedef void (*MsgP)(const char*, ...);
@@ -312,19 +197,19 @@ LUA_FUNCTION(shoom) {
 	ul_o_url = new Shm{ "ul_o_url", 512 };
 	ul_o_url->Create();
 	LOG("Initializing ul_i_image");
-	ul_i_image = new Shm{ "ul_i_image", (size_t)x * y * 4 };
+	ul_i_image = new Shm{ "ul_i_image", (size_t)(512 * 512 * 4 )+1 };
 	LOG("Initialized shoom");
 	return 0;
 }
 LUA_FUNCTION(UpdateRenderResult) {
 	ul_i_image->Open();
-
+	LUA->PushSpecial(GarrysMod::Lua::SPECIAL_GLOB);
 	LUA->GetField(-1, "surface");
 	uint32_t i = 0;
 	uint8_t* address = ul_i_image->Data();
-	for (uint16_t y = 0; y < 250; y++) //https://github.com/danielga/sourcesdk-minimal/blob/403f18104139472451a2b3518973fadeaf9691cf/tier1/interface.cpp#L472
+	for (uint16_t y = 0; y < 512; y++) //https://github.com/danielga/sourcesdk-minimal/blob/403f18104139472451a2b3518973fadeaf9691cf/tier1/interface.cpp#L472
 	{ // https://github.com/danielga/sourcesdk-minimal/blob/403f18104139472451a2b3518973fadeaf9691cf/tier1/interface.cpp#L423
-		for (uint16_t x = 0; x < 250; x++)
+		for (uint16_t x = 0; x < 512; x++)
 		{
 			LUA->GetField(-1, "SetDrawColor");
 			LUA->PushNumber(address[i]); //     R
@@ -341,6 +226,7 @@ LUA_FUNCTION(UpdateRenderResult) {
 			LUA->Call(4, 0);
 		}
 	}
+	address = nullptr;
 	LUA->Pop();
 	Msg("c++: Render end\n");
 	return 0;
@@ -357,24 +243,24 @@ GMOD_MODULE_OPEN()
 	LUA->PushCFunction(SetUrl);
 	LUA->SetField(-2, "SetUrl");
 
-	LUA->CreateTable(); // {
+	//LUA->CreateTable(); // {
 
 	LUA->PushNumber(1);
 	LUA->SetField(-2, "delay");
 
 	LUA->PushCFunction(shoom);
-	LUA->SetField(-2, "shoom");
+	LUA->SetField(-2, "ul_shoom");
 
 	LUA->PushCFunction(UpdateRenderResult);
-	LUA->SetField(-2, "UpdateRenderResult");
+	LUA->SetField(-2, "ul_UpdateRenderResult");
 
 	LUA->PushCFunction(StartRendererThread);
-	LUA->SetField(-2, "StartRendererThread");
+	LUA->SetField(-2, "ul_StartRendererThread");
 
 	LUA->PushCFunction(test);
 	LUA->SetField(-2, "test");
 
-	LUA->SetField(-2, "ultralight"); // }
+	//LUA->SetField(-2, "ultralight"); // }
 
 	LUA->GetField(-1, "SERVER");
 	if (LUA->GetBool(-1)) {
--- src/ultralight_renderer.cpp
+++ src/ultralight_renderer.cpp
@@ -20,125 +20,10 @@
 #include <windows.h> // CreateFileMapping
 #include <io.h>
 #endif
+
+#include "shoom/shm.h"
+
 using namespace ultralight;
-
-enum ShoomError {
-	kOK = 0,
-	kErrorCreationFailed = 100,
-	kErrorMappingFailed = 110,
-	kErrorOpeningFailed = 120,
-};
-class Shm {
-	std::string path_;
-	uint8_t* data_ = nullptr;
-	size_t size_ = 0;
-#if defined(_WIN32)
-	HANDLE handle_;
-#else
-	int fd_ = -1;
-#endif
-public:
-	//Shm(std::string path, size_t size) : path_(path), size_(size) {};
-	Shm(std::string path, size_t size) {
-#ifdef WIN32
-		path_ = path;
-#else
-		path_ = "/" + path;
-#endif
-		size_ = size;
-	};
-	ShoomError Create() { return CreateOrOpen(true); }
-	ShoomError Open() { return CreateOrOpen(false); }
-	uint8_t* Data() { return data_; }
-	ShoomError CreateOrOpen(bool create) {
-#ifdef WIN32
-		if (create) {
-			DWORD size_high_order = 0;
-			DWORD size_low_order = static_cast<DWORD>(size_);
-			handle_ = CreateFileMappingA(INVALID_HANDLE_VALUE,  // use paging file
-				NULL,                  // default security
-				PAGE_READWRITE,        // read/write access
-				size_high_order, size_low_order,
-				path_.c_str()  // name of mapping object
-			);
-			if (!handle_) {
-				return kErrorCreationFailed;
-			}
-		}
-		else {
-			handle_ = OpenFileMappingA(FILE_MAP_READ,  // read access
-				FALSE,          // do not inherit the name
-				path_.c_str()   // name of mapping object
-			);
-
-			if (!handle_) {
-				return kErrorOpeningFailed;
-			}
-		}
-		DWORD access = create ? FILE_MAP_ALL_ACCESS : FILE_MAP_READ;
-		data_ = static_cast<uint8_t*>(MapViewOfFile(handle_, access, 0, 0, size_));
-		if (!data_) {
-			return kErrorMappingFailed;
-		}
-		return kOK;
-#else
-		if (create) {
-			int ret = shm_unlink(path_.c_str());
-			if (ret < 0) {
-				if (errno != ENOENT) {
-					return kErrorCreationFailed;
-				}
-			}
-		}
-		int flags = create ? (O_CREAT | O_RDWR) : O_RDONLY;
-		fd_ = shm_open(path_.c_str(), flags, 0755);
-		if (fd_ < 0) {
-			if (create) {
-				return kErrorCreationFailed;
-			}
-			else {
-				return kErrorOpeningFailed;
-			}
-		}
-		if (create) {
-			int ret = ftruncate(fd_, size_);
-			if (ret != 0) {
-				return kErrorCreationFailed;
-			}
-		}
-		int prot = create ? (PROT_READ | PROT_WRITE) : PROT_READ;
-		auto memory = mmap(nullptr,     // addr
-			size_,       // length
-			prot,        // prot
-			MAP_SHARED,  // flags
-			fd_,         // fd
-			0            // offset
-		);
-		if (memory == MAP_FAILED) {
-			return kErrorMappingFailed;
-		}
-		data_ = static_cast<uint8_t*>(memory);
-		if (!data_) {
-			return kErrorMappingFailed;
-		}
-		return kOK;
-#endif
-	}
-	~Shm() {
-#ifndef WIN32
-		munmap(data_, size_);
-		close(fd_);
-		shm_unlink(path_.c_str());
-#else
-		if (data_) {
-			UnmapViewOfFile(data_);
-			data_ = nullptr;
-		}
-		CloseHandle(handle_);
-#endif
-	}
-};
-
 
 class App : public LoadListener {
 	bool done = false;
@@ -151,9 +36,9 @@ public:
 		config.font_family_standard = "Arial";
 		Platform::instance().set_config(config);
 		renderer = Renderer::Create();
-		view = renderer->CreateView(4096, 4096, false); // https://github.com/ultralight-ux/Ultralight/issues/257#issuecomment-636330995
+		view = renderer->CreateView(2048, 2048, false); // https://github.com/ultralight-ux/Ultralight/issues/257#issuecomment-636330995
 		view->set_load_listener(this);
-		view->Resize(1024, 1024);
+		view->Resize(512, 512);
 	}
 	void SetURL(String url) {
 		view->LoadURL(url);
@@ -163,10 +48,13 @@ public:
 		renderer = nullptr;
 	}
 	void Run() {
+		uint32_t timeout = 0;
 		done = false;
-		while (!done) {
+		while (timeout < 1000000000 && !done) {
+			timeout++;
 			renderer->Update();
 		}
+		std::cout << timeout << std::endl;
 	}
 	void OnFinishLoading(View* caller) {
 		done = true;
@@ -180,25 +68,24 @@ char* url = "https://github.com";
 int main(int argc, char* argv[]) {
 	App app;
 	Shm ul_io_rpc{ "ul_io_rpc", 64 };
-	Shm ul_i_image{ "ul_i_image", 1024 * 1024 * 4 };
+	Shm ul_i_image{ "ul_i_image", (512 * 512 * 4) + 1 };
 	Shm ul_o_url{ "ul_o_url", 512 };
 	ul_i_image.Create();
 	while (true) {
-		std::cout << "lop" << std::endl;
 		std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // sleep :D
 		ul_io_rpc.Open();
 		ul_o_url.Open();
-		if (ul_io_rpc.Data() != nullptr && ul_io_rpc.Data()[0] != 0) break; // stop renderer
+		if (ul_io_rpc.Data() == nullptr || ul_io_rpc.Data()[0] != 0) break; // stop renderer
 		if (ul_o_url.Data() != nullptr) {
 			std::cout << (char*)ul_o_url.Data() << std::endl;
-			if ((uint8_t*)url != ul_o_url.Data()) {
+			if (url != (char*)ul_o_url.Data()) {
 				url = (char*)ul_o_url.Data();
 				app.SetURL(url);
 				app.Run();
 				std::cout << "app->Run - succeful" << std::endl;
 				try {
 					std::cout << app.view->bitmap()->size() << std::endl;
-					memcpy(ul_i_image.Data(), app.view->bitmap()->LockPixels(), app.view->bitmap()->size() / 4); // https://stackoverflow.com/questions/2963898/faster-alternative-to-memcpy
+					memcpy(ul_i_image.Data(), (uint8_t*)app.view->bitmap()->LockPixels(), 512 * 512 * 4); // https://stackoverflow.com/questions/2963898/faster-alternative-to-memcpy
 					app.view->bitmap()->UnlockPixels();
 				}
 				catch (std::exception e) {
