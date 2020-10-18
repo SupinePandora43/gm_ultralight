8fe3f693e196d2bb5412bf7ac616c539593d63a6
SupinePandora43 <supinepandoraggg@gmail.com>
2020-10-18 17:40:01 +0500

    other binaries

--- linux.sh
+++
@@ -1,5 +0,0 @@
-mkdir bin
-curl -L https://github.com/SupinePandora43/gm_ultralight/releases/download/bin/libAppCore.so -o bin/libAppCore.so
-curl -L https://github.com/SupinePandora43/gm_ultralight/releases/download/bin/libUltralight.so -o bin/libUltralight.so
-curl -L https://github.com/SupinePandora43/gm_ultralight/releases/download/bin/libUltralightCore.so -o bin/libUltralightCore.so
-curl -L https://github.com/SupinePandora43/gm_ultralight/releases/download/bin/libWebCore.so -o bin/libWebCore.so
--- macos.sh
+++
@@ -1,5 +0,0 @@
-mkdir bin
-curl -L https://github.com/SupinePandora43/gm_ultralight/releases/download/bin/libAppCore.dylib -o bin/libAppCore.dylib
-curl -L https://github.com/SupinePandora43/gm_ultralight/releases/download/bin/libUltralight.dylib -o bin/libUltralight.dylib
-curl -L https://github.com/SupinePandora43/gm_ultralight/releases/download/bin/libUltralightCore.dylib -o bin/libUltralightCore.dylib
-curl -L https://github.com/SupinePandora43/gm_ultralight/releases/download/bin/libWebCore.dylib -o bin/libWebCore.dylib
