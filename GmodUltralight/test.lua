5e8f8c1d8124c55cb4726a4f64ad1fe249f5e063
SupinePandora43 <supinepandoraggg@gmail.com>
2020-12-27 23:15:52 +0500

    print it

--- GmodUltralight/test.lua
+++ GmodUltralight/test.lua
@@ -44,8 +44,8 @@ local function run_test()
 	
 	local dohavelines = false
 	surface = {}
-	surface.SetDrawColor = function() end
-	surface.DrawRect = function(x,y,w,h) if w~=1 then dohavelines=true end end
+	surface.SetDrawColor = function(r,g,b,a) end
+	surface.DrawRect = function(x,y,w,h) print(x,y,w,h) if w~=1 then dohavelines=true end end
 
 	print("DrawToSurfaceByLines")
 	local DrawToSurfaceByLinesStart = SysTime()
